38ec1ca29b2249f79a2eca72fa6057ce62726909
dvij542 <43860166+dvij542@users.noreply.github.com>
2021-10-18 16:11:45 +0530

    Update utils.py

--- deeprobust/image/utils.py
+++ deeprobust/image/utils.py
@@ -78,7 +78,7 @@ def make_symlink(source, link_name):
     else:
         print('Source path not exists')
 
-from texttable import Texttable
+# from texttable import Texttable
 def tab_printer(args):
     """
     Function to print the logs in a nice tabular format.
@@ -87,9 +87,9 @@ def tab_printer(args):
     """
     args = vars(args)
     keys = sorted(args.keys())
-    t = Texttable()
-    t.add_rows([["Parameter", "Value"]] +  [[k.replace("_"," ").capitalize(), args[k]] for k in keys])
-    print(t.draw())
+#     t = Texttable()
+#     t.add_rows([["Parameter", "Value"]] +  [[k.replace("_"," ").capitalize(), args[k]] for k in keys])
+#     print(t.draw())
 
 def onehot_like(a, index, value=1):
     """Creates an array like a, with all values
